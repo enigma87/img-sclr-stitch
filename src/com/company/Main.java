b27eedbcbdb5abdf51ab8b20034ab70fb34e5e36
Kishore Kumar <kkishore@athenahealth.com>
2022-01-03 19:52:23 -0500

    remove scalr, use image.getScaledInstance

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,57 +1,60 @@
 package com.company;
 
-import org.imgscalr.Scalr;
-
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriter;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File img = new File(args[0]);
-        File newImg = new File(args[0] + "_scaled.jpg");
+        File scaledImage = new File(args[0] + "_scaled.jpg");
         File stichedImage = new File(args[0] + "_stitched.png");
         int width = Integer.parseInt(args[1]);
         int height = Integer.parseInt(args[2]);
 
         Iterator<ImageWriter> imgWriter = ImageIO.getImageWritersByFormatName("jpeg");
 
-        Resize(img, newImg, width, height);
-        Stitch(img, newImg, width, height, stichedImage);
+
+        ImageIO.write(Resize(img, width, height), "jpg", scaledImage);
+        ImageIO.write(Stitch(img, scaledImage, width, height), "jpg", stichedImage);;
     }
 
-    public static void Resize(File imgFile, File newImg, int x, int y) {
+    public static BufferedImage Resize(File imgFile, int x, int y) {
         BufferedImage imgBuff;
         try {
             imgBuff = ImageIO.read(imgFile);
-            BufferedImage newImageBuff = Scalr.resize(imgBuff, Scalr.Method.BALANCED, Scalr.Mode.AUTOMATIC, x, y);
-            newImageBuff.createGraphics().drawImage(newImageBuff, 0, 0, Color.WHITE, null);
-            var bool0 = ImageIO.write(newImageBuff, "jpg", newImg);
-            newImageBuff.flush();
-            var foo = "blah";
+            Image newImage = imgBuff.getScaledInstance(x, y, Image.SCALE_DEFAULT);
+            BufferedImage scaledImage = new BufferedImage(x, y, BufferedImage.TYPE_INT_ARGB);
+            scaledImage.createGraphics().drawImage(newImage, 0, 0, Color.WHITE, null);
+            return scaledImage;
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
     }
 
-    public static void Stitch(File img1, File img2, int x, int y, File stitchImg) {
+    public static BufferedImage Stitch(File img1, File img2, int x, int y) {
         BufferedImage imgBuff1, imgBuff2;
         try {
-            imgBuff1 = Scalr.resize(ImageIO.read(img1), Scalr.Method.BALANCED, Scalr.Mode.AUTOMATIC, x, y);
-            imgBuff2 = Scalr.resize(ImageIO.read(img2), Scalr.Method.BALANCED, Scalr.Mode.AUTOMATIC, x, y);
-            BufferedImage stitchedImgBuff = new BufferedImage(x, y * 2, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g2D = stitchedImgBuff.createGraphics();
-            var bool1 = g2D.drawImage(imgBuff1, 0, 0, null);
-            var bool2 = g2D.drawImage(imgBuff2, 0, imgBuff1.getHeight(), null);
-            var bool3 = ImageIO.write(stitchedImgBuff, "png", stitchImg);
+
+            imgBuff1 = Resize(img1, x, y);
+            imgBuff2 = Resize(img2, x, y);
+
+            BufferedImage stitchedImg = new BufferedImage(x, imgBuff1.getHeight() * 2, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2D = stitchedImg.createGraphics();
+            g2D.drawImage(imgBuff1, 0, 0, null);
+            g2D.drawImage(imgBuff2, 0, imgBuff1.getHeight(), null);
             g2D.dispose();
+            return stitchedImg;
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
